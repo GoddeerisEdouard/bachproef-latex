a6c7a06d803d6e2f7a3903ea44766aa2bc479857
Goddeeris Edouard <edouard.goddeeris@student.hogent.be>
2022-08-26 11:08:23 +0200

    Prototype script

---
+++ prototype/selector_elements.py
@@ -0,0 +1,7 @@
+# automerken selectors
+car_brand_table_div_selector = "#content > main > div > div > div.mb-5"
+car_brand_ul_alfabet_letter_selector = "ul"
+
+# automodellen en bouwjaar selectors
+car_model_buildyear_element_selector = "//div[@style='font-size: 1.25em; margin: 10px !important;']"
+car_model_buildyear_text_selector = "link_grey"
